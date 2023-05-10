310d93fb1057ab4b739493ccf59a4858f8d410f2
yiminwook <grs0416@gmail.com>
2023-05-10 17:36:45 +0900

    Update: API end point

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,10 +1,9 @@
-const API_ENDPOINT =
-  "https://rhdd0roxs5.execute-api.ap-northeast-2.amazonaws.com/dev";
+const API_ENDPOINT = "http://localhost:4001";
 
 const api = {
-  fetchCats: keyword => {
-    return fetch(`${API_ENDPOINT}/api/cats/search?q=${keyword}`).then(res =>
+  fetchCats: (keyword) => {
+    return fetch(`${API_ENDPOINT}/api/cats/search?q=${keyword}`).then((res) =>
       res.json()
     );
-  }
+  },
 };
