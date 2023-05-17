64b7b80e3aeedd2457a0999e7ef2bbd678c3ec7b
yiminwook <grs0416@gmail.com>
2023-05-17 18:53:02 +0900

    Refactoring: API

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,20 +1,35 @@
 const API_ENDPOINT = "http://localhost:4001";
 
+const REQUEST_ERROR = {
+  500: { message: "500, 서버에러" },
+  400: { message: "400, 통신에러" },
+  unknown: { message: "unknown, 에러" },
+};
+
+const request = async (url) => {
+  try {
+    const response = await fetch(url);
+    const status = response.status;
+    if (status >= 200 && status < 300) return response.json();
+    else throw REQUEST_ERROR[status] || REQUEST_ERROR[unknown];
+  } catch (error) {
+    alert(error.message);
+  }
+};
+
 const api = {
-  fetchCats: (keyword) => {
-    return fetch(`${API_ENDPOINT}/api/cats/search?q=${keyword}`).then((res) =>
-      res.json()
+  fetchCats: async (keyword) => {
+    return await request(`${API_ENDPOINT}/api/cats/search?q=${keyword}`);
+  },
+  fetchCatsWithPage: async ({ keyword, page }) => {
+    return await request(
+      `${API_ENDPOINT}/api/cats/search?q=${keyword}&page=${page}`
     );
   },
-  fetchCatsWithPage: ({ keyword, page }) => {
-    return fetch(
-      `${API_ENDPOINT}/api/cats/search?q=${keyword}&page=${page}`
-    ).then((res) => res.json());
+  fetchRandomCats: async () => {
+    return await request(`${API_ENDPOINT}/api/cats/random50`);
   },
-  fetchRandomCats: () => {
-    return fetch(`${API_ENDPOINT}/api/cats/random50`).then((res) => res.json());
-  },
-  fetchCatsById: (id) => {
-    return fetch(`${API_ENDPOINT}/api/cats/${id}`).then((res) => res.json());
+  fetchCatsById: async (id) => {
+    return await request(`${API_ENDPOINT}/api/cats/${id}`);
   },
 };
